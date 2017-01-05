40ab1a51a1f342f61ec874280eee06670236a810
Mark Li <mark@philosophie.is>
2017-01-05 11:49:34 -0800

    First commit

---
+++ index.ios.js
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+export default class QrTest extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          onBarCodeRead={this.readBarCode.bind(this)}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
+
+  readBarCode(e) {
+    console.log(e.data + '------>>>> bar code read');
+  }
+
+  takePicture() {
+    console.log('-------->>>> getting logged')
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+AppRegistry.registerComponent('QrTest', () => QrTest);
